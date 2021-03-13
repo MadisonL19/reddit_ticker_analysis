3fcb64c8a19874b86b60c0cfef00b2fa5d37a9a0
Madison Leopold <leopold.madison@gmail.com>
2021-03-13 14:07:33 -0500

    Updating ticker logic and app queries

--- app.py
+++ app.py
@@ -2,19 +2,21 @@
 import praw
 import pandas as pd
 from config import client_id, client_secret, user_agent
-from tickers import Ticker, scrape_tickers, scrape_IPOs, remove_duplicates, query_list, recent_IPO_list, upcoming_IPO_list
+from tickers import Ticker, scrape_tickers, query_list, recent_IPO_list, upcoming_IPO_list
 
 # Scrape to get updated tickers and company names for stocks and ETFs
 scrape_tickers()
 
-queries = []
+import_lists = [query_list, recent_IPO_list, upcoming_IPO_list]
+
+tickers = []
 
 # Build raw query list with ticker abbrevation and name of company
-for query in query_list:
-    ticker = "$" + query.abbrev
-    queries.append(ticker)
-    queries.append(query.abbrev)
-    queries.append(query.name)
+for i in import_lists:
+    for item in i:
+        if item.abbrev == 'A':
+            item.abbrev = '-'
+        tickers.append(item)
 
 # Create connection to Reddit application
 reddit = praw.Reddit(client_id=client_id,      # your client id
@@ -32,49 +34,53 @@ subreddit_list = ['Stocks']
 for subreddit in subreddit_list:
     subreddit = reddit.subreddit(subreddit)
     # Loop through query list to query subreddit with each keyword
-    for q in queries[:500]:  # slice to first 500 queries for testing purposes
+    for ticker in tickers:
         # Loop through submission results from keyword query
         # Change time filter as needed - set to hour for testing purposes
-        for submission in subreddit.search(q, sort="top", time_filter="hour"):
-            submission_obj = {
-                "Post_Type": "Submission",
-                "Submission_ID": submission.id,
-                "Title": submission.title,
-                "Author": submission.author,
-                "Body": submission.selftext,
-                "Distinguished": submission.distinguished,
-                "Num_Comments": submission.num_comments,
-                "Name": submission.name,
-                "Permalink": submission.permalink,
-                "URL": submission.url,
-                "Score": submission.score,
-                "Upvote_Ratio": submission.upvote_ratio,
-                "Created_Date_UTC": [submission.created_utc]
-            }
-            print(submission_obj)
-            submissions_list.append(submission_obj)
-            submission.comments.replace_more(limit=0)
-            submission.comments_sort = "top"
-            # Limit number of comments return during testing
-            submission.comment_limit = 100
-            comments = submission.comments.list()
-            # Loop through commments on each submission
-            for comment in comments:
-                comment_obj = {
-                    "Post_Type": "Comment",
+        for submission in subreddit.search(ticker.abbrev, sort="hot", time_filter="day"):
+            if submission.id not in submissions_list and submission.upvote_ratio >= 0.6:
+                submission_obj = {
+                    "Ticker": ticker.abbrev,
+                    "Ticker_Type": ticker.label,
+                    "Post_Type": "Submission",
                     "Submission_ID": submission.id,
-                    "Comment_ID": comment.id,
-                    "Parent_Comment_ID": comment.parent_id,
-                    "Author": comment.author,
-                    "Body": [comment.body],
-                    "Distinguished": comment.distinguished,
-                    "Is_Author": comment.is_submitter,
-                    "Permalink": comment.permalink,
-                    "Score": comment.score,
-                    "Created_Date_UTC": [comment.created_utc]
+                    "Title": submission.title,
+                    "Author": submission.author,
+                    "Body": submission.selftext,
+                    "Distinguished": submission.distinguished,
+                    "Num_Comments": submission.num_comments,
+                    "Post_ID": submission.name,
+                    "URL": submission.url,
+                    "Score": submission.score,
+                    "Upvote_Ratio": submission.upvote_ratio,
+                    "Created_Date_UTC": [submission.created_utc]
                 }
-                print(comment_obj)
-                comments_list.append(comment_obj)
+                submissions_list.append(submission_obj)
+                submission.comments.replace_more(limit=0)
+                submission.comments_sort = "top"
+                # Limit number of comments return during testing
+                submission.comment_limit = 100
+                comments = submission.comments.list()
+                # Loop through commments on each submission
+                for comment in comments:
+                    comment_obj = {
+                        "Ticker": ticker.abbrev,
+                        "Ticker_Type": ticker.label,
+                        "Post_Type": "Comment",
+                        "Submission_ID": submission.id,
+                        "Comment_ID": comment.id,
+                        "Parent_ID": comment.parent_id,
+                        "Author": comment.author,
+                        "Body": [comment.body],
+                        "Distinguished": comment.distinguished,
+                        "Is_Author": comment.is_submitter,
+                        "Score": comment.score,
+                        "Created_Date_UTC": [comment.created_utc]
+                    }
+                    # print(comment_obj)
+                    comments_list.append(comment_obj)
+        else:
+            pass
 
 # print(submissions_list)
 # print(comments_list)
--- tickers.py
+++ tickers.py
@@ -6,8 +6,10 @@ from bs4 import BeautifulSoup
 
 
 class Ticker():
-    def __init__(self, abbrev, name):
+    def __init__(self, label, abbrev, name):
+        self.label = label
         self.abbrev = abbrev
+        self.symbol = "$" + abbrev
         self.name = name
 
 
@@ -15,14 +17,22 @@ query_list = []
 recent_IPO_list = []
 upcoming_IPO_list = []
 
+# Function to scrape all stocks, ETFs, and IPOs - calls functions
+
+
+def scrape_tickers():
+    scrape_stocks()
+    scrape_IPOs()
+
 
 # Scrape for Stocks and ETFs
-def scrape_tickers():
+def scrape_stocks():
 
     # Enter page URL where you will scrape tickers and company names from
     urls = ["http://stockanalysis.com/stocks",
             "https://stockanalysis.com/etf"]
 
+    counter = 1
     for url in urls:
         req = Request(url, headers={'User-Agent': 'Chrome/88.0.4324.192'})
         # Open the webpage
@@ -37,13 +47,25 @@ def scrape_tickers():
         tickers = ticker_list.find_all("li")
 
         for ticker in tickers:
+            if counter == 1:
+                ticker_label = "Stock"
+            else:
+                ticker_label = "ETF"
             ticker_text = ticker.text.strip()
             split_ticker = ticker_text.split(" - ")
-            query_list.append(Ticker(split_ticker[0], split_ticker[1]))
+            query_list.append(
+                Ticker(ticker_label, split_ticker[0], split_ticker[1]))
+
+    counter = counter + 1
+
+
+def scrape_IPOs():
+    scrape_recent_IPOs()
+    scrape_upcoming_IPOs()
 
 
 # Scrape for recent and upcoming IPOs
-def scrape_IPOs():
+def scrape_recent_IPOs():
 
     # Enter page URL where you will scrape recent IPOs from
     url = "https://stockanalysis.com/ipos/"
@@ -61,6 +83,8 @@ def scrape_IPOs():
     # Grab cells for each row
     recent_IPO_items = recent_IPO_table.find_all("td")
 
+    ticker_label = "Recent IPO"
+
     # Using a counter to determine which fields are pulled and saved
     counter = 1
     for item in recent_IPO_items:
@@ -69,12 +93,17 @@ def scrape_IPOs():
             counter = counter + 1
         elif counter == 3:
             item_name = item.text
-            recent_IPO_list.append(Ticker(item_ticker, item_name))
+            recent_IPO_list.append(
+                Ticker(ticker_label, item_ticker, item_name))
             counter = counter + 1
         elif counter == 6:
             counter = 1
         else:
             counter = counter + 1
+
+    # remove duplicates from IPO list if found in stock list
+    remove_duplicates()
+
 
 # Function to remove tickers from recent IPOs if found on stock page
 
@@ -93,3 +122,45 @@ def remove_duplicates():
         IPO_abbrev = IPO.abbrev
         if IPO_abbrev in query_abbrev:
             recent_IPO_list.remove(IPO)
+
+# Function to scrape upcoming IPOs
+
+
+def scrape_upcoming_IPOs():
+
+    # Enter page URL where you will scrape recent IPOs from
+    url = "https://stockanalysis.com/ipos/calendar/"
+
+    req = Request(url, headers={'User-Agent': 'Chrome/88.0.4324.192'})
+    # Open the webpage
+    page = urlopen(req)
+
+    # Pass webpage to BeautifulSoup
+    soup = BeautifulSoup(page, 'html.parser')
+
+    # Find list of tickers
+    upcoming_IPO_tables = soup.find_all("table", class_="maintable")
+
+    ticker_label = "Upcoming IPO"
+
+    for table in upcoming_IPO_tables:
+        # Grab cells for each row
+        upcoming_IPO_items = table.find_all("td")
+        counter = 1
+        for item in upcoming_IPO_items:
+            if counter == 2:
+                item_ticker = item.text
+                counter = counter + 1
+            elif counter == 3:
+                item_name = item.text
+                upcoming_IPO_list.append(
+                    Ticker(ticker_label, item_ticker, item_name))
+                counter = counter + 1
+            elif counter == 6:
+                counter = 1
+            else:
+                counter = counter + 1
+        # Using a counter to determine which fields are pulled and saved
+
+
+scrape_tickers()
